a6b2b71b6f58b1a3dbfd2b15ba7d8637a4ce8d7c
2high4dis <gamedroid2003@gmail.com>
2023-04-07 10:08:26 +0300

    employee_base

--- app/employee_base_app/views.py
+++ app/employee_base_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Employee
-from django.urls import reverse
-from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from django.db.models import Q
 
